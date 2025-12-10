8dd66966a1bdde61a4a454eefc0be35d25d4a1ad
alth45 <153092056+alth45@users.noreply.github.com>
2025-12-10 11:32:06 +0700

    fix: add reviews type definition

--- app/context/CompareContext.tsx
+++ app/context/CompareContext.tsx
@@ -4,6 +4,7 @@ import React, { createContext, useContext, useState, ReactNode } from 'react';
 import toast from 'react-hot-toast';
 
 type Product = {
+  reviews?: number;
   id: number;
   name: string;
   price: string;
